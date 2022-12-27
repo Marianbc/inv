4f8030c84e6e1dfa5288212128f31b68d36c7d9c
Marianbc <75284012+Marianbc@users.noreply.github.com>
2022-12-27 00:43:46 -0800

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -5,7 +5,7 @@ var blockingItem = "";
 var ctxMenuHolder;
 var marketLastMoney = false;
 var lastItemSold = "";
-console.log("inventory edited!");
+console.log("inventory edited! v11");
 var lockedSlots = [];
 
 if(typeof useVars === "undefined")
